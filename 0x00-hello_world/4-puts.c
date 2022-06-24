b322bb9101aa5a3e50b9cec1a1ccc2cfc2487a25
Sanctus-Peter <sanctuspeter@gmail.com>
2022-06-24 10:32:07 -0700

    C codes using puts and printf function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+        char string[100];
+        strcpy(string, "Programming is like building a multilingual puzzle");
+        puts(string);
+        return 0;
+}
