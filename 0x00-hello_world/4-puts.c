61dab223e5541fe6dfedd7683f0ab099d52dafac
Sanctus-Peter <sanctuspeter@gmail.com>
2022-06-24 10:44:46 -0700

    fixing betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - main block
+ * return: 0
+ */
 
 int main(void)
-
 {
         char string[100];
-        strcpy(string, "Programming is like building a multilingual puzzle");
+        strcpy(string, "Programming is like building a multilingual puzzle\n");
+
         puts(string);
-        return 0;
+        return (0);
 }
