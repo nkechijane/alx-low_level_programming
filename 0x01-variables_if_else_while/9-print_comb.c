73668cb481200f057fffbe5449127e81ea361236
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-01 05:50:51 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - output all single digit number of base 10
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+	{
+		putchar((var % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
