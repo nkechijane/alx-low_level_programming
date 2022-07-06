769be390da8452382ac2046161e83b0871b5a9c1
root <janecutie89@gmail.com>
2022-07-05 21:56:57 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - this program prints _putchar
+ *
+ * created by Jane
+ *
+ * Return: (0) if successful
+ */
+int main(void)
+{
+char var[] = "_putchar";
+int ii = 0;
+int lenVar = sizeof(var) / sizeof(var[0]);
+while (ii != '\0')
+for (ii = 0; ii < lenvar -1; ii++)
+{
+_putchar(var[ii]);
+ii++;
+}
+_putchar('\n')
+}
