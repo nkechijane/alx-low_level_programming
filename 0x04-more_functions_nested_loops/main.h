d86f140bac8b0b5029f6bdd5f605b22315e92558
root <janecutie89@gmail.com>
2022-07-07 15:40:51 -0700

    Adding main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
 #endif
