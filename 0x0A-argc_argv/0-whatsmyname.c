d9714c731bccd4a847bc4ff708d9bccb0e803e48
root <janecutie89@gmail.com>
2022-07-25 08:41:16 -0700

    Adding Whats my name file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of command line argument passed
+ * @argv: array of command line arguments passed
+ * Return: Always (0) on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A-argc_argv
+
+This is a project for Arguemens to main
