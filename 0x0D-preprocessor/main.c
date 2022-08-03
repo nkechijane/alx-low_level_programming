1691c8c193a814ca9525a0612558bc668ab0aa74
root <janecutie89@gmail.com>
2022-08-02 18:06:04 -0700

    Adding main.c

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,14 @@
+ines (11 sloc)  150 Bytes
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
